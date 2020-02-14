cb22254cfc4c9beefcfc27276fb9f6cdeceacf24
Dmitrii Rumyancev <rychanya@gmail.com>
2020-02-14 20:01:35 +0300

    add xml

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import xml.etree.ElementTree as ET
 
 
 def load_descriptions_from_json():
@@ -7,22 +8,39 @@ def load_descriptions_from_json():
     return [item['description'] for item in data['rss']['channel']['items']]
 
 
+def load_descriptions_from_xml():
+    tree = ET.parse('newsafr.xml')
+    descriptions = tree.findall('channel/item/description')
+    return [description.text for description in descriptions]
+
+
 def count_words(descriptions):
     words = {}
     for description in descriptions:
-        description_words = filter(lambda word: len(word) > 6, description.split())
+        description_words = filter(
+            lambda word: len(word) > 6, 
+            description.split())
         for word in description_words:
             words.setdefault(word, 0)
             words[word] += 1
-    return [(word, count) for word, count in words.items()]
+    return words
 
 
 def slice_ten_high_frequency(words):
-    sorted_words = sorted(words, key=lambda word: word[1], reverse=True)
+    sorted_words = sorted(words, key=words.get, reverse=True)
     return sorted_words[:10]
 
 
+def first_ten(descriptions):
+    words = count_words(descriptions)
+    most_wanted_words = slice_ten_high_frequency(words)
+    return most_wanted_words
+
+
 if __name__ == '__main__':
-    des = load_descriptions_from_json()
-    cw = count_words(des)
-    print(slice_ten_high_frequency(cw))
+    descriptions_from_json = load_descriptions_from_json()
+    descriptions_from_xml = load_descriptions_from_xml()
+    print('Самые часто встречаемые слова в json')
+    print(first_ten(descriptions_from_json))
+    print('Самые часто встречаемые слова в xml')
+    print(first_ten(descriptions_from_xml))
